3f1ba40ab4a35a94a72730346af09a5d62c0a6ae
BURAK ÇAĞLAYAN <burakkcaglayann@gmail.com>
2025-01-19 12:24:30 +0300

    Add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import Home from "@/pages/home";
+import { Routes, Route } from "react-router";
 import "./App.css";
 
 const App = () => {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="*" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
