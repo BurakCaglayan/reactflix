de543d03b095cf0af6035d87e03abc7cd6fe9b09
BURAK ÇAĞLAYAN <burakkcaglayann@gmail.com>
2025-01-20 00:14:22 +0300

    Add detail component & Use detail data

---
+++ src/components/Detail/index.jsx
@@ -0,0 +1,221 @@
+import {
+  Container,
+  GridColumn,
+  Grid,
+  Image,
+  Divider,
+  GridRow,
+  StatisticGroup,
+  Button,
+  ButtonGroup,
+  Statistic,
+  StatisticValue,
+  StatisticLabel,
+  Icon,
+} from "semantic-ui-react";
+import splitGenres from "@/utils/helpers";
+
+const Detail = ({ data }) => {
+  return (
+    <Container>
+      <h1>{data.Title}</h1>
+      <Divider />
+      <Grid>
+        <GridRow>
+          <GridColumn mobile={16} tablet={8} computer={4}>
+            <Image src={data.Poster} fluid rounded />
+          </GridColumn>
+          <GridColumn mobile={16} tablet={8} computer={12}>
+            <h3>Type: {data.Type}</h3>
+            <span>{data.Plot}</span>
+            <Divider />
+            <StatisticGroup widths="three" size="tiny">
+              {data.Ratings?.map((rate, index) => (
+                <Statistic key={`rate_${index}`} color="orange">
+                  <StatisticValue>{rate.Value}</StatisticValue>
+                  <StatisticLabel>{rate.Source}</StatisticLabel>
+                </Statistic>
+              ))}
+            </StatisticGroup>
+            <Divider />
+            <Grid>
+              <GridRow>
+                <GridColumn
+                  verticalAlign="middle"
+                  textAlign="right"
+                  mobile={4}
+                  tablet={4}
+                  computer={2}
+                >
+                  <h4>Duration:</h4>
+                </GridColumn>
+                <GridColumn
+                  verticalAlign="middle"
+                  textAlign="left"
+                  mobile={12}
+                  tablet={12}
+                  computer={3}
+                >
+                  <span>{data.Runtime}</span>
+                </GridColumn>
+                <GridColumn
+                  verticalAlign="middle"
+                  textAlign="right"
+                  mobile={4}
+                  tablet={4}
+                  computer={2}
+                >
+                  <h4>Genre:</h4>
+                </GridColumn>
+                <GridColumn
+                  verticalAlign="middle"
+                  textAlign="left"
+                  mobile={12}
+                  tablet={12}
+                  computer={9}
+                >
+                  <ButtonGroup size="tiny">
+                    {splitGenres(data.Genre)?.map((genre, index) => (
+                      <Button key={`genre_${index}`}>{genre}</Button>
+                    ))}
+                  </ButtonGroup>
+                </GridColumn>
+                <GridColumn
+                  verticalAlign="middle"
+                  textAlign="right"
+                  mobile={4}
+                  tablet={4}
+                  computer={2}
+                >
+                  <h4>Year:</h4>
+                </GridColumn>
+                <GridColumn
+                  verticalAlign="middle"
+                  textAlign="left"
+                  mobile={12}
+                  tablet={12}
+                  computer={3}
+                >
+                  <Button size="tiny">{data.Year}</Button>
+                </GridColumn>
+                <GridColumn
+                  verticalAlign="middle"
+                  textAlign="right"
+                  mobile={4}
+                  tablet={4}
+                  computer={2}
+                >
+                  <h4>Director:</h4>
+                </GridColumn>
+                <GridColumn
+                  verticalAlign="middle"
+                  textAlign="left"
+                  mobile={12}
+                  tablet={12}
+                  computer={9}
+                >
+                  <span>{data.Director}</span>
+                </GridColumn>
+                <GridColumn
+                  verticalAlign="middle"
+                  textAlign="right"
+                  mobile={4}
+                  tablet={4}
+                  computer={2}
+                >
+                  <h4>Country:</h4>
+                </GridColumn>
+                <GridColumn
+                  verticalAlign="middle"
+                  textAlign="left"
+                  mobile={12}
+                  tablet={12}
+                  computer={14}
+                >
+                  <span>{data.Country}</span>
+                </GridColumn>
+                <GridColumn
+                  verticalAlign="middle"
+                  textAlign="right"
+                  mobile={4}
+                  tablet={4}
+                  computer={2}
+                >
+                  <h4>Language:</h4>
+                </GridColumn>
+                <GridColumn
+                  verticalAlign="middle"
+                  textAlign="left"
+                  mobile={12}
+                  tablet={12}
+                  computer={14}
+                >
+                  <span>{data.Language}</span>
+                </GridColumn>
+                <GridColumn
+                  verticalAlign="middle"
+                  textAlign="right"
+                  mobile={4}
+                  tablet={4}
+                  computer={2}
+                >
+                  <h4>Actors:</h4>
+                </GridColumn>
+                <GridColumn
+                  verticalAlign="middle"
+                  textAlign="left"
+                  mobile={12}
+                  tablet={12}
+                  computer={14}
+                >
+                  <span>{data.Actors}</span>
+                </GridColumn>
+                <GridColumn
+                  verticalAlign="middle"
+                  textAlign="right"
+                  mobile={4}
+                  tablet={4}
+                  computer={2}
+                >
+                  <h4>Writer:</h4>
+                </GridColumn>
+                <GridColumn
+                  verticalAlign="middle"
+                  textAlign="left"
+                  mobile={12}
+                  tablet={12}
+                  computer={14}
+                >
+                  <span>{data.Writer}</span>
+                </GridColumn>
+                <GridColumn
+                  verticalAlign="middle"
+                  textAlign="right"
+                  mobile={4}
+                  tablet={4}
+                  computer={2}
+                >
+                  <h4>
+                    <Icon name="trophy" />
+                    Awards:
+                  </h4>
+                </GridColumn>
+                <GridColumn
+                  verticalAlign="middle"
+                  textAlign="left"
+                  mobile={12}
+                  tablet={12}
+                  computer={14}
+                >
+                  <span>{data.Awards}</span>
+                </GridColumn>
+              </GridRow>
+            </Grid>
+          </GridColumn>
+        </GridRow>
+      </Grid>
+    </Container>
+  );
+};
+
+export default Detail;
--- src/pages/detail/index.jsx
+++ src/pages/detail/index.jsx
@@ -1,12 +1,24 @@
+import { useEffect } from "react";
 import { useParams } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
 import Header from "@/components/Header";
+import Detail from "@/components/Detail";
+import { getMovieByIdRequest } from "@/api";
 
 const DetailPage = () => {
   const { id } = useParams();
+  const dispatch = useDispatch();
+  const { data } = useSelector((state) => state.movieDetails);
+
+  useEffect(() => {
+    const params = { id };
+    dispatch(getMovieByIdRequest({ params }));
+  }, [dispatch]);
+
   return (
     <>
       <Header />
-      <div>DetailPage {id}</div>
+      <Detail id={id} data={data} />
     </>
   );
 };
